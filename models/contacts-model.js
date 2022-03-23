81f4b557834a6e2d6164e6056f27c5d5ba0d04f9
alexgndeveloper <alexgn26developer@gmail.com>
2022-03-23 21:45:09 +0100

    Change object without version

--- models/contacts-model.js
+++ models/contacts-model.js
@@ -2,13 +2,18 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // Definimos el Modelo Contacto
-const contactSchema = new Schema({
-  name: { type: String, required: [true, 'El nombre de la persona es obligatorio'] },
-  phone: { type: String, required: false },
-  cellphone: { type: String, required: [true, 'El número de teléfono es obligatorio'] },
-  email: { type: String, required: false },
-  address: { type: String, required: false },
-});
+const contactSchema = new Schema(
+  {
+    name: { type: String, required: [true, 'El nombre de la persona es obligatorio'] },
+    phone: { type: String, required: false },
+    cellphone: { type: String, required: [true, 'El número de teléfono es obligatorio'] },
+    email: { type: String, required: false },
+    address: { type: String, required: false },
+  },
+  {
+    versionKey: false
+  }
+);
 
 // Exportamos el Modelo Contacto
 module.exports = mongoose.model('Contact', contactSchema);
