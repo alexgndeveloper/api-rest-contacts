4477498d603fac3e55c1e38648ff9808bc4fa9d0
alexgndeveloper <alexgn26developer@gmail.com>
2022-03-28 21:48:36 +0200

    Change object contact

--- models/contacts-model.js
+++ models/contacts-model.js
@@ -5,15 +5,24 @@ const Schema = mongoose.Schema;
 const contactSchema = new Schema(
   {
     name: { type: String, required: [true, 'El nombre de la persona es obligatorio'] },
+    surname: { type: String, required: false },
     phone: { type: String, required: false },
     cellphone: { type: String, required: [true, 'El número de teléfono es obligatorio'] },
     email: { type: String, required: false },
-    address: { type: String, required: false },
+    address: [
+      {
+        direction: { type: String, required: false },
+        number: { type: Number, required: false },
+        floor: { type: String, required: false },
+        province: { type: String, required: false }
+      }
+    ]
   },
   {
     versionKey: false
   }
 );
+
 
 // Exportamos el Modelo Contacto
 module.exports = mongoose.model('Contact', contactSchema);
